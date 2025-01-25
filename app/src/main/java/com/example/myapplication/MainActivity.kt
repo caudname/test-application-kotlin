77e3a6d148c0648330d7cd53b68d5ec14c676cae
said <khasbulatov.vniigaz@gmail.com>
2025-01-25 10:51:09 +0300

    Increased font size

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,6 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -60,13 +66,24 @@ fun Counter() {
     ) {
         Text(
             text = "Количество: $count",
-            modifier = Modifier.padding(bottom = 16.dp),
+            modifier = Modifier.padding(bottom = 48.dp),
+            fontSize = 24.sp
             )
-        Button(onClick = { count++ }) {
-            Text(text = "ОК")
-        }
-        Button(onClick = { count = 0 }) {
-            Text(text = "Сбросить")
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+        ) {
+            Button(
+                onClick = { count++ },
+                modifier = Modifier.size(width = 140.dp, height = 60.dp),
+            ) {
+                Text(text = "ОК", fontSize = 20.sp)
+            }
+            Button(onClick = { count = 0 },
+                modifier = Modifier.size(width = 140.dp, height = 60.dp),
+            ) {
+                Text(text = "Сбросить", fontSize = 20.sp)
+            }
         }
     }
 }
